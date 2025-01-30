a6a027e833dbcd789a6e213bc1826b2ca99e4abd
Yerson Arango <yersonarango159@gmail.com>
2025-01-30 17:52:42 +0100

    Delete Proyecto Final directory

--- Proyecto Final/app.py
+++
@@ -1,8 +0,0 @@
-from models import *
-import db
-
-if __name__ == "__main__":
-    db.Base.metadata.create_all(db.engine)
-    app_screen = LogInScreen()
-
-
--- Proyecto Final/db.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-engine = create_engine("sqlite:///database/usuarios.db",
-                       connect_args={'check_same_thread': False})
-
-Session = sessionmaker(bind=engine)
-session = Session()
-Base = declarative_base()
